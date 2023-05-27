007f540dfa0fec41983fcf98126dfc044c12b7e4
KingWahidullah <fausharifi@gmail.com>
2023-05-26 20:11:24 -0700

    again

--- src/GitStdies/GitClass.java
+++ src/GitStdies/GitClass.java
@@ -1,10 +1,19 @@
 package GitStdies;
 
 public class GitClass {
+	
 
 	public static void main(String[] args) {
 		
-		String name ="king";
+		String fname="AliSharifi";
+		
+		System.out.println(fname);
+		
+	
+		
+			
+
+		
 		
 	}
 }
