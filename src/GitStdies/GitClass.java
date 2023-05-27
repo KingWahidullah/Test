5779a639f7662934b086e75ed5dd835213937808
KingWahidullah <fausharifi@gmail.com>
2023-05-26 20:14:39 -0700

    haha

--- src/GitStdies/GitClass.java
+++ src/GitStdies/GitClass.java
@@ -5,12 +5,7 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		
-		String fname="AliSharifi";
-		
-		System.out.println(fname);
-		
-		int age= 22;
-		System.out.println(fname+ "\nage is "+age);
+		System.out.println("my name is wahidullah sharifi");
 		
 	
 		
