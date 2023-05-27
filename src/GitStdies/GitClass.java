1848cd8edf27cdb9dfa8089a272aa08fdc3141e5
KingWahidullah <fausharifi@gmail.com>
2023-05-26 20:46:08 -0700

    ss

--- src/GitStdies/GitClass.java
+++ src/GitStdies/GitClass.java
@@ -1,16 +1,33 @@
 package GitStdies;
 
+import java.util.Iterator;
+
 public class GitClass {
 	
 
 	public static void main(String[] args) {
 		
-		System.out.println("my name is wahidullah sharifi");
+		String name="my name is wahidullah sharifi";
+		String rev="";
+		String nM=name.substring(11,21);
 		
+		for(int i =nM.length()-1;i>=0;i--) {
+			rev+=nM.charAt(i);
+		
+		}	System.out.println(name);
+		System.out.println(nM);
+		System.out.println(rev);
+		
+		String wow="khan";
+		
+		
+		StringBuilder kk= new StringBuilder(wow);
 	
+		kk=kk.reverse();
+		System.out.println(wow + " is now : " + kk);
 		
-			
-
+		
+   System.out.println("goooooooooooooooood");
 		
 		
 	}
